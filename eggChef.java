9823eb378ece12b914e890fcaf6020b5fbafd25c
BlairSweigman <blair.sweigman@gmail.com>
2016-01-07 20:17:14 -0500

    working buttons

--- eggChef.java
+++ eggChef.java
@@ -1,26 +1,39 @@
 import java.util.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+
+/**
+* Egg Chef application
+*
+* @author Blair Sweigman
+* @version 1.0
+*/
 class eggChef {
 	JFrame frame;
 	JPanel cartonPanel;
 	JPanel cookPanel;
+	JTextField text;
+	int eggIndex;
+	ArrayList<eggPanel> epl;
 	public static void main(String[] args) {
 		eggChef  ec = new eggChef();
 		Carton c = new Carton(12);
 		ec.buildGUI();
 	}	
 		public void buildGUI() {
+			eggIndex=0;
 		frame = new JFrame();
 		
 		frame.setSize(800,800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridLayout grid = new GridLayout(6,2);
 		cartonPanel = new JPanel(grid);
-			ArrayList<eggPanel> epl = new ArrayList<eggPanel>() ;
+			 epl = new ArrayList<eggPanel>() ;
 		for (int i=0;i<12;i++) {
 	    	eggPanel ep = new eggPanel();
 			cartonPanel.add(ep);
+			epl.add(ep);
 			
 		}
 		cartonPanel.setPreferredSize(new Dimension(280, 400));
@@ -28,16 +41,71 @@ class eggChef {
 		cookPanel = new JPanel();
 		cookPanel.setLayout(new BoxLayout(cookPanel,BoxLayout.Y_AXIS));
 		JButton buttonO = new JButton("Omelete");
+		buttonO.addActionListener(new makeOmelete());
 		cookPanel.add(buttonO);
 		JButton buttonH = new JButton("Hard Boiled");
+		buttonH.addActionListener(new makeHardBoiled());
 		cookPanel.add(buttonH);
 		JButton buttonS = new JButton("Soft Boiled");
+		buttonS.addActionListener(new makeSoftBoiled());
 		cookPanel.add(buttonS);
+		
+		JButton buttonX = new JButton("Exit");
+
+		buttonX.addActionListener(new exitFrame());
+		cookPanel.add(buttonX);
+
 		frame.add(BorderLayout.WEST,cookPanel);
+		text = new JTextField();
+		frame.add(BorderLayout.SOUTH,text);
 		frame.setBounds(50,50,500,500);
 		frame.pack();
 		frame.repaint();
 		frame.setVisible(true);
 	}
 
+	class EggMaker {
+		protected void makeEgg(String status, String sText,int eggCount) {
+			if (eggIndex<epl.size()) {
+				text.setText( sText );
+				epl.get(eggIndex).setStatus(status);
+				eggIndex++;
+				frame.repaint();
+			}
+			else {
+				text.setText( "OUT OF EGGS!");
+			}
+		}
+	}
+	class makeOmelete extends EggMaker implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+                makeEgg("EMPTY","Made an Omelete",1);
+                //text.setText("Made an Omelete");
+                
+                //epl.get(eggIndex).setStatus("EMPTY");
+                //frame.repaint();
+                //eggIndex++;
+
+        }
+	}
+	class makeHardBoiled extends EggMaker implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+               makeEgg("HARD","Made a Hard Boiled Egg",1);
+               // text.setText("Made a Hard Boiled Egg");
+                //epl.get(eggIndex).setStatus("HARD");
+                //frame.repaint();
+                //eggIndex++;
+        }
+	}
+	class makeSoftBoiled extends EggMaker implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+                
+               makeEgg("SOFT","Made a Soft Boiled Egg",1);
+        }
+	}
+	class exitFrame implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			frame.dispose();
+		}
+	}
 }
--- eggPanel.java
+++ eggPanel.java
@@ -1,11 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
 class eggPanel extends JPanel {
-	
+	private String eggStatus;
+ 	public eggPanel() {
+ 		eggStatus = "RAW";
+ 	}	
+ 	public void setStatus(String inStatus) {
+ 		eggStatus = inStatus;
+ 	}
 	public  void paintComponent(Graphics g) {
 	    g.fillRect(0,0,this.getWidth(),this.getHeight());
-		g.setColor(Color.white);
+		g.setColor(getColor());
 		g.fillOval(5,5,25,35);
+	}
+	private Color getColor() {
+		Color retCol;
+		switch (eggStatus) {
+			case "RAW":
+				retCol = Color.white;
+				break;
+			case "HARD":
+				retCol = Color.yellow;
+				break;
+			case "SOFT":
+				retCol = Color.pink;
+				break;
+			case "EMPTY":
+				retCol = Color.darkGray;	
+			    break;
+			default:
+				retCol = Color.red;
+				break;
+		}
+		return retCol;
+		
 	}
 }
 	
